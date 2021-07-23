9d3065b7bafe81914cb52259ea9c6b07428c0d18
Bisratemichael Moges <bisrat.moges306@gmail.com>
2021-07-23 14:23:01 +0300

    Listed all heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import{ Hero }from '../hero';
+import { HEROES } from '../moch-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,6 +12,11 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
+  heroes=HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
   constructor() { }
 
   ngOnInit(): void {
